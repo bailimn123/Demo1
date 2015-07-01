0f56657e4de549edf600d7ae7500b74e883dd2fd
tarena <tarena@1111111.local>
2015-07-01 11:15:57 +0800

    aaa

---
+++ aaaa/aaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaa
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaa/aaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaa
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
